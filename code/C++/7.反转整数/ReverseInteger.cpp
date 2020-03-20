fca36effc10415864c835dbc2e5a0ec6727c7d6b
bqw <476448856@qq.com>
2020-03-20 13:25:57 +0800

    反转整数

---
+++ code/C++/7.反转整数/ReverseInteger.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int reverse(int x) {
+    int rev = 0;
+    while(x!=0){
+        int pop = x % 10;
+        x /= 10;
+        // ÅĞ¶ÏÒç³öÇé¿ö
+        if (rev > INT_MAX/10 || (rev == INT_MAX / 10 && pop > 7)) return 0;
+        if (rev < INT_MIN/10 || (rev == INT_MIN / 10 && pop < -8)) return 0;
+        rev = rev * 10 + pop;
+    }
+    return rev;
+}
+
+int main(){
+    cout<<reverse(-123456);
+    return 0;
+}
